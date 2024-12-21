20d8f7dabbc60e263ff855eed633f5dbd1d27260
GuzidYagmur <guzideyagmur@hotmail.com>
2024-12-21 19:49:46 +0300

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,8 +1,34 @@
-const ages= [12,15,45,86,53,21,46,75,92];
+const ages= [
+    {
+        id:1,
+        name:"Yağmur",
+        age:54
+    },
+    {
+        id:2,
+        name:"Furkan",
+        age:46
+    },
+    {
+        id:3,
+        name:"Sevde",
+        age:12
+    },
+    {
+        id:4,
+        name:"Orhan",
+        age:34
+    },
+    {
+        id:5,
+        name:"Ahmet",
+        age:64
+    }
+];
 
 function checkAge(agesArray){
    
-    const checkAdult = agesArray.filter(age => age>18);
+    const checkAdult = agesArray.filter(person => person.age>18);
     return checkAdult;
 
 }
